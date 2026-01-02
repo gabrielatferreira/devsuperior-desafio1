c45f820c197644924e87378fb195c788c0ce87df
Gabriela Trindade Ferreira <gabitriferreira@gmail.com>
2026-01-02 15:05:31 -0300

    add order entities and services for price calculation and shipping rules

--- src/main/java/com/devsuperior/desafio1/Desafio1Application.java
+++ src/main/java/com/devsuperior/desafio1/Desafio1Application.java
@@ -1,12 +1,21 @@
 package com.devsuperior.desafio1;
 
 import com.devsuperior.desafio1.entities.Order;
+import com.devsuperior.desafio1.service.OrderService;
+import com.devsuperior.desafio1.service.ShippingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 @SpringBootApplication
 public class Desafio1Application implements CommandLineRunner {
+
+	@Autowired
+	private OrderService orderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Desafio1Application.class, args);
@@ -14,7 +23,23 @@ public class Desafio1Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Order order = new Order();
+		Scanner le = new Scanner(System.in);
+		le.useLocale(Locale.US);
+
+		System.out.println("Digite o código do pedido:");
+		int code = le.nextInt();
+		System.out.println("Digite o valor básico:");
+		double basic = le.nextDouble();
+		System.out.println("Digite o desconto:");
+		double discount = le.nextDouble();
+
+		Order order = new Order(code, basic, discount);
+		double total = orderService.total(order);
+
 		System.out.println("Pedido código " + order.getCode());
+		System.out.printf(Locale.US,"Valor total: R$ %.2f%n", total);
+
+		le.close();
+		System.exit(0);
 	}
 }
--- src/main/java/com/devsuperior/desafio1/service/OrderService.java
+++ src/main/java/com/devsuperior/desafio1/service/OrderService.java
@@ -1,4 +1,19 @@
 package com.devsuperior.desafio1.service;
 
+import com.devsuperior.desafio1.entities.Order;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
+
+    public final ShippingService shippingService;
+
+    public OrderService(ShippingService shippingService) {
+        this.shippingService = shippingService;
+    }
+
+    public double total(Order order) {
+        double discountValue = order.getBasic() * (order.getDiscount() / 100.0);
+        return order.getBasic() - discountValue + shippingService.shipment(order);
+    }
 }
